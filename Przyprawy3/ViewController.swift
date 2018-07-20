687b0fe5e1d6cb69974f19c1b4aeb80a629c23e4
kurczewski7 <slawomir.kurczewski@gmail.com>
2018-07-20 17:31:01 +0200

    Initial Commit

---
+++ Przyprawy3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Przyprawy3
+//
+//  Created by Slawek Kurczewski on 20.07.2018.
+//  Copyright © 2018 Slawomir Kurczewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Przyprawy3Tests/Przyprawy3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Przyprawy3Tests.swift
+//  Przyprawy3Tests
+//
+//  Created by Slawek Kurczewski on 20.07.2018.
+//  Copyright © 2018 Slawomir Kurczewski. All rights reserved.
+//
+
+import XCTest
+@testable import Przyprawy3
+
+class Przyprawy3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
