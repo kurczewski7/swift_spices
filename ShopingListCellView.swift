67e4eb7be0cdfc6001890de486067668ab28ad88
kurczewski7 <slawomir.kurczewski@gmail.com>
2018-08-21 16:31:27 +0200

    CollectionView added.

---
+++ ShopingListCellView.swift
@@ -0,0 +1,15 @@
+//
+//  ShopingListCellView.swift
+//  Przyprawy3
+//
+//  Created by Slawek Kurczewski on 21.08.2018.
+//  Copyright © 2018 Slawomir Kurczewski. All rights reserved.
+//
+
+import UIKit
+
+class ShopingListCellView: UICollectionViewCell {
+    @IBOutlet var productPicture: UIImageView!
+    @IBOutlet var productName: UILabel!
+    
+}
---
+++ ShopingListViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ShopingListViewController.swift
+//  Przyprawy3
+//
+//  Created by Slawek Kurczewski on 21.08.2018.
+//  Copyright © 2018 Slawomir Kurczewski. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "collectionCell"
+
+class ShopingListViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        //self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+     func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return picturesArray.count
+    }
+
+     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ShopingListCellView
+    
+        // Configure the cell
+        cell.backgroundView?.backgroundColor = UIColor.brown
+        cell.productName.text = "aaa"
+        cell.productPicture.image = UIImage(named: picturesArray[indexPath.row])
+        
+        
+        
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
